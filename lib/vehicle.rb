958b9f95c8fb6b566ff7442232ea7426b3f0eea7
Asia Kane and Dana Weiss <asia@>
2017-04-26 08:55:47 -0700

    add specs for make, model, and year

--- lib/vehicle.rb
+++ lib/vehicle.rb
@@ -8,4 +8,21 @@ class Vehicle
   define_method(:age) do
     Time.now().year()-@year
   end
+
+  define_method(:make) do
+    @make
+  end
+
+  define_method(:model) do
+    @model
+  end
+
+  define_method(:year) do
+    @year
+  end
+
+  define_method(:worth_buying?) do
+    american_cars = ["Chrysler", "Ford", "GM"]
+    age()<16 && american_cars.include?()
+  end
 end
--- spec/vehicle_spec.rb
+++ spec/vehicle_spec.rb
@@ -8,4 +8,32 @@ describe(Vehicle) do
       expect(test_vehicle.age()).to(eq(17))
     end
   end
+
+  describe('#worth_buying?') do
+    it('returns false if vehicle is not american made and younger than 16') do
+      test_vehicle = Vehicle.new('Toyota','Prius',2000)
+      expect(test_vehicle.worth_buying?()).to(eq(false))
+    end
+  end
+
+  describe('#make') do
+    it('returns the make of the vehicle') do
+      test_vehicle = Vehicle.new('Toyota','Prius',2000)
+      expect(test_vehicle.make()).to(eq('Toyota'))
+    end
+  end
+
+  describe('#model') do
+    it('returns the model of the vehicle') do
+      test_vehicle = Vehicle.new('Toyota','Prius',2000)
+      expect(test_vehicle.model()).to(eq('Prius'))
+    end
+  end
+
+  describe('#year') do
+    it('returns the year of the vehicle') do
+      test_vehicle = Vehicle.new('Toyota','Prius',2000)
+      expect(test_vehicle.year()).to(eq(2000))
+    end
+  end
 end
