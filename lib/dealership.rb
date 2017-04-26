1d35a0279de485f50ccc2d120ac6c6a6a21c428e
Asia Kane and Dana Weiss <asia@>
2017-04-26 10:10:13 -0700

    add methods and passing tests for dealership all, save, and clear

--- lib/dealership.rb
+++ lib/dealership.rb
@@ -4,6 +4,7 @@ class Dealership
   define_method(:initialize) do |name|
     @name = name
     @id = @@dealerships.length()+1
+    @cars = []
   end
 
   define_method(:name) do
@@ -12,5 +13,21 @@ class Dealership
 
   define_method(:id) do
     @id
+  end
+
+  define_method(:cars) do
+    @cars
+  end
+
+  define_singleton_method(:all) do
+    @@dealerships
+  end
+
+  define_method(:save) do
+    @@dealerships.push(self)
+  end
+
+  define_singleton_method(:clear) do
+    @@dealerships = []
   end
 end
--- spec/dealership_spec.rb
+++ spec/dealership_spec.rb
@@ -3,6 +3,10 @@ require "rspec"
 require "pry"
 
 describe(Dealership) do
+  before() do
+    Dealership.clear()
+  end
+
   describe('#name') do
     it('returns the dealership name') do
       test_dealership = Dealership.new("Bob's Cars")
@@ -17,18 +21,33 @@ describe(Dealership) do
     end
   end
 
+  describe('#cars') do
+    it('returns an empty array of cars for the dealership') do
+      test_dealership = Dealership.new("Bob's Cars")
+      expect(test_dealership.cars()).to(eq([]))
+    end
+  end
 
+  describe('#save') do
+    it('adds a dealership to the array of dealerships') do
+      test_dealership = Dealership.new("Bob's Cars")
+      test_dealership.save()
+      expect(Dealership.all()).to(eq([test_dealership]))
+    end
+  end
 
+  describe('.all') do
+    it('empty at first') do
+      expect(Dealership.all()).to(eq([]))
+    end
+  end
 
-
-
-  # before() do
-  #   Dealership.clear()
-  # end
-  #
-  # describe('.clear') do
-  #   it('will clear the dealerships array') do
-  #     test_dealer
-  #   end
-  # end
+  describe('.clear') do
+    it('will clear the dealerships array') do
+      test_dealership = Dealership.new("Bob's Cars")
+      test_dealership.save()
+      Dealership.clear()
+      expect(Dealership.all()).to(eq([]))
+    end
+  end
 end
